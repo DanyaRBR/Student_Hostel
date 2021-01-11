164d45fa11bddfbe960e674df518377627bb93dd
DanyaRBR <74045447+DanyaRBR@users.noreply.github.com>
2021-01-12 03:58:43 +0700

    Refactoring

--- StudentsHostel/ManageStudents.h
+++ StudentsHostel/ManageStudents.h
@@ -6,24 +6,28 @@ class User;
 namespace Ui {
 class ManageStudents;
 }
-
+/*!
+    \brief Класс управления студентами.
+    Данное окно содержит в себе список студентов и кнопки для управления студентами.
+*/
 class ManageStudents : public QDialog
 {
     Q_OBJECT
 
 public:
-
+    /*!
+    Конструктор класса главного окна
+    \param userlist Список студентов
+    \param parent ссылка на родительский объект
+    */
     explicit ManageStudents(const QList<User> &userlist, QWidget *parent = nullptr);
     ~ManageStudents();
 
 public slots:
 
 private slots:
-
     void on_add_clicked();
-
     void on_del_clicked();
-
     void on_edit_clicked();
 
 private:
@@ -31,9 +35,9 @@ private:
     const QList<User> &s_userlist;
 
 signals:
-    void addStudent(User);
-    void editStudent(int, User);
-    void deleteStudent(int);
+    void addStudent(User); ///<Сигнал отправляемый при добавлении нового студента, посылает добавленного студента
+    void editStudent(int, User); ///<Сигнал отправляемый при редактировании студента, посылает номер редактируемой строки, а также новые данные о студенте
+    void deleteStudent(int); ///<Сигнал отправляемый при удалении студента, посылает номер удаляемой строки
 
 };
 #endif // MANAGESTUDENTS_H
