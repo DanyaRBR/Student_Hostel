c91fe31d4343047213b0e5a4e14b863ca281367b
DanyaRBR <74045447+DanyaRBR@users.noreply.github.com>
2021-01-12 03:57:26 +0700

    Refactoring

--- StudentsHostel/ManagePersonnel.h
+++ StudentsHostel/ManagePersonnel.h
@@ -6,24 +6,27 @@ class User;
 namespace Ui {
 class ManagePersonnel;
 }
-
+/*!
+    \brief Класс управления персоналом.
+    Данный класс предназначен для управления персоналом.
+*/
 class ManagePersonnel : public QDialog
 {
     Q_OBJECT
 
 public:
-
+    /*!
+    Конструктор класса главного окна
+    \param parent ссылка на родительский объект
+    */
     explicit ManagePersonnel(QWidget *parent = nullptr);
     ~ManagePersonnel();
 
 public slots:
 
 private slots:
-
     void on_add_clicked();
-
     void on_del_clicked();
-
 private:
     Ui::ManagePersonnel *ui;
     QList<User> s_userlist;
